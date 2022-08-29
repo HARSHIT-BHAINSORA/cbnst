a790a49bf7a5150f686fd733f77d260e5b74d929
Harshit Bhainsora <90299672+HARSHIT-BHAINSORA@users.noreply.github.com>
2022-08-29 09:39:00 +0530

    error Dedection in polynomial equation

---
+++ Lab1/Program1.c
@@ -0,0 +1,31 @@
+// WAP IN C TO DEDUCE ERROR  IN Involved ANY POLYNOMIAL EQUATION .
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+	float abError , reError , perError , t_val , a_val;
+	
+	printf("Enter Ture value = ");	
+	scanf("%f" , &t_val);
+	
+	printf("Enter approx value = ");	
+	scanf("%f" , &a_val);
+	
+	abError = t_val - a_val;
+	
+	reError = abError / t_val;
+	
+	perError = reError * 100;
+
+	
+	printf("The absolute Error is = %f \n" , abError);
+	printf("The absolute Error is = %f  \n" , reError);
+	printf("The absolute Error is = %f \n" , perError);
+	
+	return 0;
+		
+	
+	
+}
